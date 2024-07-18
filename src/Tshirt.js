a19bcddcadc0476787047cddad66b921001fc96b
jobinijohnson <jobiny.j@squarely.co>
2024-07-18 16:29:11 +0530

    canvas changes

---
+++ src/Tshirt.js
@@ -0,0 +1,12 @@
+// import React from 'react'
+// import { App as Canvas } from './Canvas'
+// import { Overlay } from './Overlay'
+
+// export default function Tshirt() {
+//   return (
+//     <>
+//       <Canvas />
+//       <Overlay />
+//     </>
+//   )
+// }
