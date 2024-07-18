5fbd25ab266b5a513b2d53f588cb20c54e183878
jobinijohnson07 <jobinijohnson981@gmail.com>
2024-07-18 17:47:47 +0530

    Resolved merge conflicts

--- src/Canvas.js
+++ src/Canvas.js
@@ -41,8 +41,11 @@ function CameraRig({ children }) {
 
 function Shirt(props) {
   const snap = useSnapshot(state)
-  const texture = useTexture(`/${snap.decal}.png`)
-  const { nodes, materials } = useGLTF('/shirt_baked_collapsed.glb')
+  // const texture = useTexture(`/${snap.decal}.png`)
+  const texture = useTexture(process.env.PUBLIC_URL + '/three2.png');
+  const { nodes, materials } = useGLTF(process.env.PUBLIC_URL + '/shirt_baked_collapsed.glb');
+
+  // const { nodes, materials } = useGLTF('/shirt_baked_collapsed.glb')
   useFrame((state, delta) => easing.dampC(materials.lambert1.color, snap.color, 0.25, delta))
   return (
     <mesh castShadow geometry={nodes.T_Shirt_male.geometry} material={materials.lambert1} material-roughness={1} {...props} dispose={null}>
